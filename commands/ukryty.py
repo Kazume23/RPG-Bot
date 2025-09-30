8f36b7c4df77db79e3fca838015304c4d9b666e8
Kazume <mcwicio@gmail.com>
2025-09-30 16:23:01 +0200

    Stable version

--- commands/ukryty.py
+++ commands/ukryty.py
@@ -2,13 +2,11 @@ from services.rolls import roll_logic
 from services.dm_sender import send_admin_dm
 
 
-async def ukryty_command(message, ctx):
-    parts = message.split(maxsplit=1)
+async def ukryty_command(ctx):
+    parts = ctx.content.split(maxsplit=1)
     if len(parts) < 2:
         return "Ty chuju. Pisz jak człowiek np: 5d6"
 
-    print(ctx)
-    print(type(ctx))
-
     result = roll_logic(parts[1])
     await send_admin_dm(ctx.bot, f"User {ctx.author} rolled: {parts[1]}\n{result}")
+    return f"Rzuciłeś ukryty rzut lampucero"
