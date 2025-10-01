2bd0fd9a6d80204bb38d7b67e9c060ec331a4d11
Kazume <mcwicio@gmail.com>
2025-10-01 22:00:48 +0200

    Make exception to admin in DM

--- commands/utility.py
+++ commands/utility.py
@@ -1,2 +1,10 @@
+import os
+
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+
+
 def has_admin_permissions(ctx):
-    return ctx.author.guild_permissions.administrator
+    if ctx.guild:
+        return ctx.author.guild_permissions.administrator
+    else:
+        return ctx.author.id == ADMIN_ID
